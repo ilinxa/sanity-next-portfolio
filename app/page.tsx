2d53f7eb7ec0bc30b1f94da3fe3357b44e1cc630
HessamHezaveh <hessamhezaveh@gmail.com>
2025-01-11 17:31:48 +0300

    small edits tabs color

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,19 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 const Home = () => {
 
   return (
-    <div className="h-full w-full flex flex-col justify-center items-center ">
+    <div className="h-full  flex flex-col justify-center items-center w-screen">
       <div className="flex flex-col  gap-20 mb-16">
 
         <Herosection/>
       </div>
 
-      <div className="mt-10 max-w-5xl z-[100] mx-10 min-h-screen">
+      <div className="mt-10 max-w-5xl z-[100] mx-10 min-h-screen w-full">
       <Tabs defaultValue="webDevelopment" className="">
         <TabsList className="flex w-full h-full mb-10 ">
-          <TabsTrigger value="webDevelopment" className="text-wrap h-fit w-full " >Web & Application</TabsTrigger>
+          <TabsTrigger value="webDevelopment" className="text-wrap h-fit w-full  " >Web & Application</TabsTrigger>
           <TabsTrigger value="videoEditing" className="text-wrap h-full w-full " >Video and Editing</TabsTrigger>
           <TabsTrigger value="motionContent" className="text-wrap h-full w-full" >3D Motion Content</TabsTrigger>
-          <TabsTrigger value="forSale" className="text-wrap h-full w-[30%] bg-primary text-white" >For Sale</TabsTrigger>
+          <TabsTrigger value="forSale" className="text-wrap h-full w-[30%] bg-primary color-white" >For Sale</TabsTrigger>
         </TabsList>
 
         <TabsContent value="webDevelopment">
